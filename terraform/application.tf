b6a9c62bc69aef02e3b436dc95796d13d07ff19e
root <root@TemitopeDlight.localdomain>
2026-02-09 16:22:17 +0100

    Disable Helm wait to prevent timeout on micro nodes

--- terraform/application.tf
+++ terraform/application.tf
@@ -6,11 +6,15 @@ resource "kubernetes_namespace" "retail_app" {
 
 resource "helm_release" "retail_store" {
   name       = "retail-store"
-  # FIX: Point to the parent registry, not the full chart path. 
-  # Terraform appends the chart name automatically.
   repository = "oci://public.ecr.aws/aws-containers"
   chart      = "retail-store-sample-chart"
   namespace  = kubernetes_namespace.retail_app.metadata[0].name
   
+  # --- THE FIX ---
+  # Don't fail if pods take too long to start on tiny nodes
+  wait       = false 
+  timeout    = 900 # Increase timeout to 15 minutes just in case
+  # ---------------
+
   depends_on = [module.eks]
 }
